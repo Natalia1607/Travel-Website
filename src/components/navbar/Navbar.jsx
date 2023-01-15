42b1dbdb228e08f7c99c7ef1af2277aa2c13e132
Natalia Gilyazova <gilyazovan97@mail.ru>
2023-01-15 16:04:35 +0300

    Mobile menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import { BsPerson } from 'react-icons/bs';
 import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 import './NavbarStyles.css';
 
 const Navbar = () => {
+  const [ nav, setNav ] = useState(false);
+  const handleNav = () => setNav(!nav);
+
   return (
-    <div className='navbar'>
-      <div className="logo">
-        <h2>BEACHES/</h2>
+    <div className={nav ? 'navbar navbar-bg' : 'navbar'}>
+      <div className='logo'>
+        <h2>BEACHES.</h2>
       </div>
-      <ul className="nav-menu">
+      <ul className='nav-menu'>
         <li>Home</li>
         <li>Destinations</li>
         <li>Travel</li>
         <li>Book</li>
         <li>Views</li>
       </ul>
-      <div className="nav-icons">
-        <BiSearch className='icon' />
+      <div className='nav-icons'>
+        <BiSearch className='icon' style={{marginRight: '1rem'}}/>
         <BsPerson className='icon' />
       </div>
-      <div className="hamburger">
+      <div className='hamburger' onClick={handleNav}>
         <HiOutlineMenuAlt4 className='icon' />
       </div>
 
-      <div className="mobile-menu">
+      <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
         <ul className="mobile-nav">
           <li>Home</li>
           <li>Destinations</li>
@@ -34,6 +38,19 @@ const Navbar = () => {
           <li>Book</li>
           <li>Views</li>
         </ul>
+        <div className='mobile-menu-bottom'>
+          <div className='menu-icons'>
+            <button>Search</button>
+            <button>Account</button>
+          </div>
+          <div className='social-icons'>
+              <FaFacebook className='icon' />
+              <FaInstagram className='icon' />
+              <FaTwitter className='icon' />
+              <FaPinterest className='icon' />
+              <FaYoutube className='icon' />
+          </div>
+        </div>
       </div>
     </div>
   )
